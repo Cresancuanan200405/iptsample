f4e3a0f3e7a0de5802b4f003ccf8773c5333cff6
Cresancuanan200405 <cresan.cuanan@urios.edu.ph>
2025-09-03 17:49:25 +0800

    a

--- NationalAnthem/Lyrics.py
+++ NationalAnthem/Lyrics.py
@@ -1,4 +1,27 @@
 #Bayang Magiliw, Perlas ng Silanganan
 #Alab ng Puso sa dibdib mo'y buhay
 #Lupang Hinirang, Duyan ka ng magiting,
+<<<<<<< Updated upstream
 #Sa manlulupig di ka pasisiil.
+=======
+#Sa manlulupig di ka pasisiil.
+
+#Sa dagat at bundok,
+#Sa simoy at sa langit mong bughaw,
+#May dilag ang tula at awit
+#Sa paglayang minamahal.
+
+#Ang kislap ng watawat mo'y
+#Tagumapay na nagniningning 
+# Ang bituin at araw niya
+# Kailan pa ma'y di magdidilim 
+
+#Lupa ng araw, ng luwalhati't pagsinta
+#Buhay ay langit sa piling mo
+#Aming ligaya na 'pag may mang-aapi
+#Ang mamatay ng dahil sa 'yo
+
+#That's all
+
+what is livesdfmkvd
+>>>>>>> Stashed changes
