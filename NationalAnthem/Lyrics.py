4ddff4b7cb228e89bcf041030c3215b50c0704be
Cresancuanan200405 <cresan.cuanan@urios.edu.ph>
2025-09-03 15:39:00 +0800

    verse2new

--- NationalAnthem/Lyrics.py
+++
@@ -1,4 +0,0 @@
-#Bayang Magiliw, Perlas ng Silanganan
-#Alab ng Puso sa dibdib mo'y buhay
-#Lupang Hinirang, Duyan ka ng magiting,
-#Sa manlulupig di ka pasisiil.
---
+++ NationalAnthem/Verse2.py
@@ -0,0 +1,4 @@
+#Bayang Magiliw, Perlas ng Silanganan
+#Alab ng Puso sa dibdib mo'y buhay
+#Lupang Hinirang, Duyan ka ng magiting,
+#Sa manlulupig di ka pasisiil.
