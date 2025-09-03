bd544fbcf8d27e492b1a5ab7bb2a3b8e7372d3b9
Cresancuanan200405 <cresan.cuanan@urios.edu.ph>
2025-09-03 17:25:38 +0800

    losdf

--- NationalAnthem/Lyrics.py
+++ NationalAnthem/Lyrics.py
@@ -1,20 +1,18 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #Lyrics of the Philippine National Anthem
 
 #Bayang Magiliw, Perlas ng Silanganan
 #Alab ng Puso sa dibdib mo'y buhay
 #Lupang Hinirang, Duyan ka ng magiting,
 #Sa manlulupig di ka pasisiil.
-=======
+
 #Sa dagat at bundok,
 #Sa simoy at sa langit mong bughaw,
 #May dilag ang tula at awit
 #Sa paglayang minamahal.
->>>>>>> Verse3
-=======
+
 #Ang kislap ng watawat mo'y
 #Tagumapay na nagniningning 
 # Ang bituin at araw niya
 # Kailan pa ma'y di magdidilim 
->>>>>>> Verse4
+
+#
