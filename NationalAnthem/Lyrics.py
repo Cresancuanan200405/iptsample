6eabd7d86a80a7e0d36a6985624ac71956e0ade1
Cresancuanan200405 <cresan.cuanan@urios.edu.ph>
2025-09-03 17:33:20 +0800

    new

--- NationalAnthem/Lyrics.py
+++ NationalAnthem/Lyrics.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Lyrics of the Philippine National Anthem
 
 #Bayang Magiliw, Perlas ng Silanganan
@@ -16,10 +15,9 @@
 # Ang bituin at araw niya
 # Kailan pa ma'y di magdidilim 
 
-#
-=======
 #Lupa ng araw, ng luwalhati't pagsinta
 #Buhay ay langit sa piling mo
 #Aming ligaya na 'pag may mang-aapi
 #Ang mamatay ng dahil sa 'yo
->>>>>>> Verse5
+
+#That's all
